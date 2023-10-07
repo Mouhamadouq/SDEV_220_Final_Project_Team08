da3311626f0a8b40d04a93f0da72b16514f23387
Rashmi Baral <rbaral@ivytech.edu>
2023-10-07 12:36:05 -0400

    uploaded an image at header section

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
+print(STATIC_ROOT)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
