1638fbede411746e404b70a8bd6d00523d0ae875
Mouhamadouq <taphisco13@gmail.com>
2023-10-13 04:21:51 -0400

    updated models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from django.conf import Settings
+from django.conf import settings
 from django.contrib.auth.models import User
 
 # Create your models here.
